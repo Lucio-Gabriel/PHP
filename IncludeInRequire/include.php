23f036b0956bb0983f599b6096356f123932ea07
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-03-05 07:23:39 -0300

    created head and  include iis index

---
+++ IncludeInRequire/include.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'head.php'; ?>    
+    <title>Include</title>
+</head>
+<body>
+    <h1><?= 'Include' ?></h1>
+</body>
+</html>
---
+++ IncludeInRequire/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include in Require</title>
+</head>
+<body>
+    
+    <h1>Include in Require</h1>
+
+    <ul>
+        <li>
+            <a href="/include.php">Include</a>
+        </li>
+
+        <li>
+            <a href="/require.php">Require</a>
+        </li>
+    </ul>
+
+</body>
+</html>
