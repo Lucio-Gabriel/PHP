ead6f717688de764c4a6fd23fa0c7d314affbf81
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-03-09 09:19:02 -0300

    created example basic

---
+++ Functions/example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div>
+        <h1>Somas</h1>
+
+        <?php 
+            function soma($a, $b) {
+                return $a + $b;
+            }
+
+            echo soma(5, 5);
+        ?>
+    </div>
+
+</body>
+</html>
---
+++ Functions/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funcoes</title>
+</head>
+<body>
+    
+    <div>
+        <h1>Funcoes</h1>
+
+        <ul>
+            <li><a href="/example.php">Exemplo basico</a></li>
+        </ul>
+    </div>
+
+</body>
+</html>
